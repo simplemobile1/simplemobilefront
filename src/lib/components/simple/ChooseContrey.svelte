fe2c126d6b29491d7cce31c2411025d35cfd5f11
baruch babayof <aviadam.segel@gmail.com>
2023-02-16 15:56:57 +0200

    with search

--- src/lib/components/simple/ChooseContrey.svelte
+++ src/lib/components/simple/ChooseContrey.svelte
@@ -1,15 +1,40 @@
 <script>
     import country from '$lib/country.json'
+    import {countries} from '$lib/components/Autocomplete/countriesData.js'
+     import MultiSelect from 'svelte-multiselect'
+let list = country
+  const ops = countries
+let matchingOptions = []
+  let selected = []
+  let open = false
+  let searchText = ``
     function contrey (uu){
         console.log(uu)
     }
+    $:if (searchText.length > 0){
+        open = false
+        console.log(searchText, matchingOptions)
+   list = country.filter(function (el)
+    {
+      return el.name.toLowerCase().includes(searchText.toLowerCase()) ;
+    }
+    )
+    }
 </script>
 <div class="w-full  p-4 bg-white   sm:p-6">
+    <MultiSelect 
+    bind:selected 
+    options={ops} 
+    bind:matchingOptions 
+    maxSelect={1}
+    bind:searchText
+    bind:open
+    />
         <ul class="my-4  flex flex-wrap justify-center ">
-    {#each country as state}
+    {#each list as state}
      <li >
                 <button on:click={()=>contrey(state.code)} class="m-1 flex items-center p-3 w-fit text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow">
-                    <img src="{state.flag_1x1}" aria-hidden="true" class="h-4"/>
+                    <img alt="{state.name}" src="{state.flag_1x1}" aria-hidden="true" class="h-4"/>
                     <span class="flex-1 ml-3 whitespace-nowrap">{state.name}</span>
                     <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded ">Popular</span>
                 </button>
