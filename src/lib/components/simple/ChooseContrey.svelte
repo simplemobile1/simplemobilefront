2390356f19db75e24949fa420d1d6897e6f21ae7
baruch babayof <aviadam.segel@gmail.com>
2023-02-16 14:40:40 +0200

    p-ip-u

--- src/lib/components/simple/ChooseContrey.svelte
+++ src/lib/components/simple/ChooseContrey.svelte
@@ -4,14 +4,14 @@
         console.log(uu)
     }
 </script>
-<div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 dark:bg-gray-800 dark:border-gray-700">
-        <ul class="my-4 space-y-3">
+<div class="w-full  p-4 bg-white   sm:p-6">
+        <ul class="my-4  flex flex-wrap justify-center ">
     {#each country as state}
-     <li>
-                <button on:click={()=>contrey(state.code)} class="flex items-center p-3 text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-white">
+     <li >
+                <button on:click={()=>contrey(state.code)} class="m-1 flex items-center p-3 w-fit text-base font-bold text-gray-900 rounded-lg bg-gray-50 hover:bg-gray-100 group hover:shadow">
                     <img src="{state.flag_1x1}" aria-hidden="true" class="h-4"/>
                     <span class="flex-1 ml-3 whitespace-nowrap">{state.name}</span>
-                    <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded dark:bg-gray-700 dark:text-gray-400">Popular</span>
+                    <span class="inline-flex items-center justify-center px-2 py-0.5 ml-3 text-xs font-medium text-gray-500 bg-gray-200 rounded ">Popular</span>
                 </button>
             </li>
     {/each}
