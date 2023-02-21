4ff31ecd73e3babd4f66cd81b48aa245999da9b0
baruch babayof <aviadam.segel@gmail.com>
2023-02-21 18:19:57 +0200

    seventh

--- src/lib/components/simple/ChooseGeo.svelte
+++ src/lib/components/simple/ChooseGeo.svelte
@@ -53,7 +53,14 @@ let matchingOptions = []
         </ul>
         </div>
 {:else}
-<button>close</button>
-<Prodtab kind="geo" {code} {esim}/>        
+<button on:click="{()=>choosed = false}" type="button" class="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span class="sr-only">Close menu</span>
+              <!-- Heroicon name: outline/x -->
+              <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+              </svg>
+            </button>
+            
+            <Prodtab kind="geo" {code} {esim}/>        
 {/if}
   
